f07b490848a53c5f88c44c0b18985787128b18ac
SatvikS7 <satviksriram7@gmail.com>
2024-05-20 01:45:33 -0700

    explore finished

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -139,7 +139,7 @@ class RecipeCard extends HTMLElement {
   		<div class="rating">
     		<span>5</span>
     		<img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
-    		<span>${data.numRatings}</span>
+    		<span>(${data.numRatings})</span>
   		</div>
   		<time>${data.lengthTime}</time>
   		<p class="ingredients">
